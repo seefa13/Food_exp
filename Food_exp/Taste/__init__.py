504aaf896879eb63840cb7cb0891b5ad7bc3d4a2
Your Name <stephan.fally@gmail.com>
2022-04-29 10:46:45 +0200

    message

--- Food_exp/Taste/__init__.py
+++ Food_exp/Taste/__init__.py
@@ -21,20 +21,86 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    taste_item1 = models.IntegerField(min=1, max=5)
-    taste_item2 = models.IntegerField(min=1, max=5)
-    taste_item3 = models.IntegerField(min=1, max=5)
-    taste_item4 = models.IntegerField(min=1, max=5)
-    taste_item5 = models.IntegerField(min=1, max=5)
-    taste_item6 = models.IntegerField(min=1, max=5)
-    taste_item7 = models.IntegerField(min=1, max=5)
-    taste_item8 = models.IntegerField(min=1, max=5)
+    taste_item1 = models.IntegerField(
+        choices=[
+            [1,'I would never like eat that'], 
+            [2,'I would most likely not like to eat that'],
+            [3,'I would sometimes like to eat that'],
+            [4,'I would most likely like to eat that'],
+            [5,'I would definitely like to eat that'],
+            ]
+        )
+    taste_item2 = models.IntegerField(
+        choices=[
+            [1,'I would never like eat that'], 
+            [2,'I would most likely not like to eat that'],
+            [3,'I would sometimes like to eat that'],
+            [4,'I would most likely like to eat that'],
+            [5,'I would definitely like to eat that'],
+            ]
+        )
+    taste_item3 = models.IntegerField(
+        choices=[
+            [1,'I would never like eat that'], 
+            [2,'I would most likely not like to eat that'],
+            [3,'I would sometimes like to eat that'],
+            [4,'I would most likely like to eat that'],
+            [5,'I would definitely like to eat that'],
+            ]
+        )
+    taste_item4 = models.IntegerField(
+        choices=[
+            [1,'I would never like eat that'], 
+            [2,'I would most likely not like to eat that'],
+            [3,'I would sometimes like to eat that'],
+            [4,'I would most likely like to eat that'],
+            [5,'I would definitely like to eat that'],
+            ]
+        )
+    taste_item5 = models.IntegerField(
+        choices=[
+            [1,'I would never like eat that'], 
+            [2,'I would most likely not like to eat that'],
+            [3,'I would sometimes like to eat that'],
+            [4,'I would most likely like to eat that'],
+            [5,'I would definitely like to eat that'],
+            ]
+        )
+    taste_item6 = models.IntegerField(
+        choices=[
+            [1,'I would never like eat that'], 
+            [2,'I would most likely not like to eat that'],
+            [3,'I would sometimes like to eat that'],
+            [4,'I would most likely like to eat that'],
+            [5,'I would definitely like to eat that'],
+            ]
+        )
+    taste_item7 = models.IntegerField(
+        choices=[
+            [1,'I would never like eat that'], 
+            [2,'I would most likely not like to eat that'],
+            [3,'I would sometimes like to eat that'],
+            [4,'I would most likely like to eat that'],
+            [5,'I would definitely like to eat that'],
+            ]
+        )
+    taste_item8 = models.IntegerField(
+        choices=[
+            [1,'I would never like eat that'], 
+            [2,'I would most likely not like to eat that'],
+            [3,'I would sometimes like to eat that'],
+            [4,'I would most likely like to eat that'],
+            [5,'I would definitely like to eat that'],
+            ]
+        )
     
 
 
 # PAGES
 class Taste_Q(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['taste_item1', 'taste_item2', 'taste_item3', 'taste_item4', 'taste_item5', 'taste_item6', 
+    'taste_item7', 'taste_item8']
 
 
 page_sequence = [Taste_Q]

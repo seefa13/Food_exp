20b5c36b9efb3619b05a8fa8f7f569289fb8588d
Your Name <stephan.fally@gmail.com>
2022-05-26 18:33:54 +0200

    adapted food list for vegans and combinations

--- Food_exp/Taste/__init__.py
+++ Food_exp/Taste/__init__.py
@@ -22,19 +22,38 @@ class C(BaseConstants):
     NAME_IN_URL = 'Taste'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
-    # list of healthy (h) and unhealthy (unh) food items
+    # list of food items
     # should be modified only here, not later on
     lFoods = [
-        'Banana','Strawberries','Chia seeds','Cashews','Salmon','Vegetable mix','White Beans','Yogurt',
-        'Whole Grain Bread','Chicken, filet','Butter, plant-based','Milk, low fat','Pork','Mueslibar, no sugar added',
-        'Water','Cottage Cheese','Boiled Egg', 'Banana chips','Strawberries, Smoothie','Sunflower seeds',
-        'Cashews, salted','Salmon, smoked','Vegetables, breaded','Baked Beans','Yogurt, sweetened','White Bread',
-        'Chicken, breaded filet','Butter','Milk, whole fat','Pork, sausage','Mueslibar, sweetened','Lemonade',
-        'Cheese','Egg, salad'
+        'a Banana',             'Strawberries',         'Chia seeds',       'Chips',                    'Salmon',
+        'a Vegetable mix',      'White Beans',          'Yogurt',           'Whole Grain Bread (vegan)','a Chickenfilet',
+        'Chocolate (vegan)',    'a Croissant (vegan)',  'Pork',             'a sugarfree Mueslibar',    'Water',
+        'Cheese (vegan)',       'a Boiled Egg',         'Banana chips',     'a Berrysmoothie (vegan)',  'Vanilla Ice Cream (vegan)',
+        'salted Cashews',       'smoked Salmon',        'Hummus',           'sweetened Cranberries',    'sweetened Yogurt (Soja)',
+        'White Bread (vegan)',  'a breaded Chicken filet','Butter (vegan)', 'Milk',                     'a Sausage (Pork)',        
+        'a sweetened Mueslibar','Lemonade',             'Cheese',           'Crackers',                 'a Soja-Drink',
+        'Ravioli Funghi',       'salted Popcorn'
     ]
 
     # prices health scores
-    lNutri = [1,1,1,2,1,1,1,1,1,1,3,1,2,1,1,2,1,4,5,3,3,4,2,3,3,2,3,5,2,4,3,3,4,3]
+    lNutri = [
+        1,1,1,4,1,
+        1,1,1,1,1,
+        5,5,2,1,1,
+        4,1,4,5,4,
+        3,4,3,3,2,
+        2,3,5,2,4,
+        3,3,4,4,1,
+        2, 4
+    ]
+    ##          A: 13 (v: 9)
+    # A: 13 
+    ##          BC: 11 (v: 9)
+    # B: 5
+    # C: 6
+    ##          DE: 13 (v: 9)
+    # D: 9
+    # E: 4
 
 
 class Subsession(BaseSubsession):
@@ -47,39 +66,50 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     iFruit = models.StringField(blank=True)
-    iFruit_unh = models.StringField(blank=True)
     iBerries = models.StringField(blank=True)
-    iBerries_unh = models.StringField(blank=True)
     iSeeds = models.StringField(blank=True)
-    iSeeds_unh = models.StringField(blank=True)
-    iNuts = models.StringField(blank=True)
-    iNuts_unh = models.StringField(blank=True)
+    iChips = models.StringField(blank=True)
     iFish = models.StringField(blank=True)
-    iFish_unh = models.StringField(blank=True)
+
     iVeg = models.StringField(blank=True)
-    iVeg_unh = models.StringField(blank=True)
     iBeans = models.StringField(blank=True)
-    iBeans_unh = models.StringField(blank=True)
     iYog = models.StringField(blank=True)
-    iYog_unh = models.StringField(blank=True)
     iBread = models.StringField(blank=True)
-    iBread_unh = models.StringField(blank=True)
     iChicken = models.StringField(blank=True)
-    iChicken_unh = models.StringField(blank=True)
-    iButter_unh = models.StringField(blank=True)
-    iButter = models.StringField(blank=True)
-    iMilk = models.StringField(blank=True)
-    iMilk_unh = models.StringField(blank=True)
+
+    iChoco = models.StringField(blank=True)
+    iCroissant = models.StringField(blank=True)
     iRedmeat = models.StringField(blank=True)
-    iRedmeat_unh = models.StringField(blank=True)   
-    iBar_unh = models.StringField(blank=True)
     iBar = models.StringField(blank=True)
     iDrink = models.StringField(blank=True)
-    iDrink_unh = models.StringField(blank=True)
-    iCheese = models.StringField(blank=True)
-    iCheese_unh = models.StringField(blank=True)
+
+    iCheese_h = models.StringField(blank=True)
     iEggs = models.StringField(blank=True)
-    iEggs_unh = models.StringField(blank=True)
+    iFruit_unh = models.StringField(blank=True)
+    iBerries_unh = models.StringField(blank=True)
+    iIce = models.StringField(blank=True)
+
+    iNuts_unh = models.StringField(blank=True)
+    iFish_unh = models.StringField(blank=True)
+    iHummus = models.StringField(blank=True)
+    iCran = models.StringField(blank=True)
+    iYog_unh = models.StringField(blank=True)
+
+    iBread_unh = models.StringField(blank=True)
+    iChicken_unh = models.StringField(blank=True)
+    iButter_unh = models.StringField(blank=True)
+    iMilk = models.StringField(blank=True)
+    iRedmeat_unh = models.StringField(blank=True)   
+
+    iBar_unh = models.StringField(blank=True)
+    iDrink_unh = models.StringField(blank=True)
+    iCheese_unh = models.StringField(blank=True)
+    iCracker = models.StringField(blank=True)
+    iSoja = models.StringField(blank=True)
+
+    iRavioli = models.StringField(blank=True)
+    iPopcorn = models.StringField(blank=True)
+
     V1 = models.StringField(blank=True)
     V2 = models.StringField(blank=True)
 
@@ -88,10 +118,14 @@ class Player(BasePlayer):
 class Taste(Page):
     form_model = 'player'
     form_fields = [
-        'iFruit','iFruit_unh','iBerries','iBerries_unh','iSeeds','iSeeds_unh','iNuts','iNuts_unh','iFish','iFish_unh',
-        'iVeg','iVeg_unh','iBeans','iBeans_unh','iYog','iYog_unh','iBread','iBread_unh','iChicken','iChicken_unh',
-        'iButter_unh','iButter','iMilk','iMilk_unh','iRedmeat','iRedmeat_unh','iBar_unh','iBar','iDrink','iDrink_unh',
-        'iCheese','iCheese_unh','iEggs','iEggs_unh','V1','V2'
+        'iFruit', 'iBerries', 'iSeeds', 'iChips', 'iFish', 
+        'iVeg', 'iBeans', 'iYog', 'iBread', 'iChicken', 
+        'iChoco', 'iCroissant', 'iRedmeat', 'iBar', 'iDrink', 
+        'iCheese_h', 'iEggs', 'iFruit_unh', 'iBerries_unh', 'iIce', 
+        'iNuts_unh', 'iFish_unh', 'iHummus', 'iCran', 'iYog_unh', 
+        'iBread_unh', 'iChicken_unh', 'iButter_unh', 'iMilk', 'iRedmeat_unh', 
+        'iBar_unh', 'iDrink_unh', 'iCheese_unh', 'iCracker', 'iSoja', 
+        'iRavioli', 'iPopcorn', 'V1','V2'
     ]
     
     @staticmethod
@@ -100,20 +134,21 @@ class Taste(Page):
         lPrevcomb = [100]
 
         # initialize lists of healthy and unhealthy food items, taste ratings, prices and health scores
-        lFoods = list(C.lFoods)
-        lTastes = [
-        int(self.iFruit),int(self.iBerries),int(self.iSeeds),int(self.iNuts),int(self.iFish),int(self.iVeg),
-        int(self.iBeans),int(self.iYog),int(self.iBread),int(self.iChicken),int(self.iButter),int(self.iMilk),
-        int(self.iRedmeat),int(self.iBar),int(self.iDrink),int(self.iCheese),int(self.iEggs),int(self.iFruit_unh),
-        int(self.iBerries_unh),int(self.iSeeds_unh),int(self.iNuts_unh),int(self.iFish_unh),int(self.iVeg_unh),
-        int(self.iBeans_unh),int(self.iYog_unh),int(self.iBread_unh),int(self.iChicken_unh),int(self.iButter_unh),
-        int(self.iMilk_unh),int(self.iRedmeat_unh),int(self.iBar_unh),int(self.iDrink_unh),int(self.iCheese_unh),
-        int(self.iEggs_unh)
+        lFoods      = list(C.lFoods)
+        lTastes     = [
+            int(self.iFruit),int(self.iBerries),int(self.iSeeds),int(self.iChips),int(self.iFish),
+            int(self.iVeg),int(self.iBeans),int(self.iYog),int(self.iBread),int(self.iChicken),
+            int(self.iChoco),int(self.iCroissant),int(self.iRedmeat),int(self.iBar),int(self.iDrink),
+            int(self.iCheese_h),int(self.iEggs),int(self.iFruit_unh),int(self.iBerries_unh),int(self.iIce),
+            int(self.iNuts_unh),int(self.iFish_unh),int(self.iHummus),int(self.iCran),int(self.iYog_unh),
+            int(self.iBread_unh),int(self.iChicken_unh),int(self.iButter_unh),int(self.iMilk),int(self.iRedmeat_unh),
+            int(self.iBar_unh),int(self.iDrink_unh),int(self.iCheese_unh),int(self.iCracker),int(self.iSoja),
+            int(self.iRavioli),int(self.iPopcorn)
         ]
-        lNutri = list(C.lNutri)
+        lNutri      = list(C.lNutri)
 
         # delete items with rating "1" in both lists
-        counter = 0
+        counter     = 0
         for value in lTastes:
             if value == 1:
                 lTastes.remove(counter)
